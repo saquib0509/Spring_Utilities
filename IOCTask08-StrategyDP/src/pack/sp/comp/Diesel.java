4f3258488fc922ceec41d1c25c51fb98ca5c634b
Saquib Akhtar <158837727+saquib0509@users.noreply.github.com>
2025-10-18 13:25:21 +0530

    Delete IOC08-StrategyDP directory

--- IOCTask08-StrategyDP/src/pack/sp/comp/Diesel.java
+++
@@ -1,9 +0,0 @@
-package pack.sp.comp;
-
-public class Diesel implements IFuel{
-	@Override
-	public String engine(String carno) {
-		
-		return carno+ " car no. is having engine of type Diesel";
-	}
-}
--- IOCTask08-StrategyDP/src/pack/sp/comp/IFuel.java
+++
@@ -1,6 +0,0 @@
-package pack.sp.comp;
-
-public interface IFuel {
-
-	public String engine(String carno);
-}
